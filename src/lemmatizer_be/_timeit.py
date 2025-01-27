f3cb052233c2957714a048a8bb8c83f10223aeba
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2025-01-27 23:21:53 +0300

    #patch memory mode

--- src/lemmatizer_be/_timeit.py
+++ src/lemmatizer_be/_timeit.py
@@ -5,6 +5,8 @@ import timeit
 from lemmatizer_be import BnkorpusLemmatizer
 
 if __name__ == "__main__":
-    lm = BnkorpusLemmatizer()
-    execution_time = timeit.timeit("lm.lemmas('перавырашаць')", globals=globals(), number=100_000)
+    lm = BnkorpusLemmatizer(db_storage="memory")
+    execution_time = timeit.timeit(
+        "lm.lemmas('перавырашаць')", globals=globals(), number=100_000
+    )
     print(f"Execution time: {execution_time:.9f} seconds")
--- src/lemmatizer_be/lemmatizer.py
+++ src/lemmatizer_be/lemmatizer.py
@@ -7,8 +7,9 @@ from __future__ import annotations
 import os
 import sqlite3
 from pathlib import Path
+from typing import Literal
 
-from lemmatizer_be._utils import _fetch_unzip, dir_empty
+from lemmatizer_be._utils import _fetch_unzip, dir_empty, singleton
 
 DATA_DIR = Path(
     os.environ.get(
@@ -25,18 +26,40 @@ DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 LEMMA_DATA_URL = "https://github.com/alex-rusakevich/lemmatizer-be/releases/latest/download/lemma_data.zip"
 
+DB_PATH = DATA_DIR / "lemma_data.sqlite3"
 
+
+@singleton
 class BnkorpusLemmatizer:
     """Belarusian language lemmatizer based on bnkorpus."""
 
-    def __init__(self):
-        """Connect to the lemma data sqlite3 db."""
+    def __init__(self, db_storage: Literal["memory", "disk"] = "memory"):
+        """Connect to the lemma data sqlite3 db.
+
+        Parameters
+        ----------
+        db_storage : Literal[&quot;memory&quot;, &quot;disk&quot;], optional
+            Where to store the lemmas database, by default "disk"
+            * "memory" is much faster than "disk", but takes some time to load and a lot of RAM.
+            * "disk" starts immediately, takes little RAM, but lemma functions
+            are slower than while using "memory" mode.
+
+        """
         if dir_empty(DATA_DIR):
             print("The lemmatizer's data is missing, downloading...")
             _fetch_unzip(LEMMA_DATA_URL, DATA_DIR)
             print("The lemmatizer's data has been downloaded successfully.")
 
-        self._conn = sqlite3.connect(str(DATA_DIR / "lemma_data.sqlite3"), check_same_thread=False)
+        if db_storage == "disk":
+            self._conn = sqlite3.connect(str(DB_PATH), check_same_thread=False)
+        elif db_storage == "memory":
+            source_conn = sqlite3.connect(str(DB_PATH))
+            self._conn = sqlite3.connect(":memory:")
+
+            source_conn.backup(self._conn)
+            source_conn.close()
+        else:
+            raise Exception("Wrong db_storage value: {}".format(db_storage))
 
     def lemmas(self, word: str, pos: str | None = None) -> list[str]:
         """Return list of all the lemmas for the word.
